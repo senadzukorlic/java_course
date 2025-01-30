18ede49ebfe137b38bea8548e40fd67188928bd6
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-01-30 17:29:09 +0100

    constructor

--- src/Hello.java
+++ src/Hello.java
@@ -1,16 +1,16 @@
 public class Hello {
     public static void main(String[] args) {
-        Car Toyota = new Car();
-        Toyota.make = "Toyota";
-        Toyota.price = 20000;
-        Toyota.year = 2020;
-        Toyota.color = "Red";
+        Car Toyota = new Car("Toyota", 20000, 2020, "Red");
+//        Toyota.make = "Toyota";
+//        Toyota.price = 20000;
+//        Toyota.year = 2020;
+//        Toyota.color = "Red";
 
-        Car Nissan = new Car();
-        Nissan.make = "Nissan";
-        Nissan.price = 25000;
-        Nissan.year = 2021;
-        Nissan.color = "Blue";
+        Car Nissan = new Car("Nissan", 25000, 2021, "Blue");
+//        Nissan.make = "Nissan";
+//        Nissan.price = 25000;
+//        Nissan.year = 2021;
+//        Nissan.color = "Blue";
 
         System.out.println("Toyota: " + Toyota.make + " " + Toyota.price + " " + Toyota.year + " " + Toyota.color);
         System.out.println("Nissan: " + Nissan.make + " " + Nissan.price + " " + Nissan.year + " " + Nissan.color);
