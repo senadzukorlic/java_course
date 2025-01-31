e3335bc94fb049a3480f4274cf481369dee3896b
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-01-31 16:22:14 +0100

    arrays and mutable objects

--- src/Car.java
+++ src/Car.java
@@ -3,19 +3,23 @@
         private double price;
         private int year;
         private String color;
+        private String[] parts;
 
-    public Car(String make, double price, int year, String color){
-        this.make = make;
-        this.price = price;
-        this.year = year;
-        this.color = color;
-    }
+        public Car(String make, double price, int year, String color, String[] parts) {
+            this.make = make;
+            this.price = price;
+            this.year = year;
+            this.color = color;
+            this.parts = parts;
+        }
 
-    public Car(Car source){
+        public Car(Car source){
         this.make = source.make;
         this.price = source.price;
         this.year = source.year;
         this.color = source.color;
+        this.parts = source.parts;
+
     }
         public void setColor(String color) {
             this.color = color;
@@ -47,6 +51,18 @@
 
         public String getColor() {
             return color;
+        }
+
+        public String[] getParts() {
+            return parts;
+        }
+
+        public void setParts(String[] parts) {
+            this.parts = parts;
+        }
+
+        public void drive(){
+        System.out.println("You bought a " + make + " " + year + " " + color + " for " + price + " dollars. Enjoy your ride!");
         }
     }
 
--- src/Hello.java
+++ src/Hello.java
@@ -1,16 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        Car Toyota = new Car("Toyota", 20000, 2020, "Red");
-        Car Nissan = new Car("Nissan", 25000, 2021, "Blue");
+        String[] spareParts = new String[]{"Engine", "Tires", "Brakes"};
+        Car Toyota = new Car("Toyota", 20000, 2020, "Red", spareParts);
+        Car Nissan = new Car("Nissan", 25000, 2021, "Blue", spareParts);
 
         Car Toyota2 = new Car(Toyota);
 
-        Toyota2.setColor("Green");
-        Toyota.setColor("Yellow");
 
-        System.out.println("Toyota: " + Toyota.getMake() + " " + Toyota.getPrice() + " " + Toyota.getYear() + " " + Toyota.getColor());
-        System.out.println("Nissan: " + Nissan.getMake() + " " + Nissan.getPrice() + " " + Nissan.getYear() + " " + Nissan.getColor());
-        System.out.println("Toyota2: " + Toyota2.getMake() + " " + Toyota2.getPrice() + " " + Toyota2.getYear() + " " + Toyota2.getColor());
-
+        Toyota.drive();
     }
 }
