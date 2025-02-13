005d414c7c6cbba1317b900daf8ed5eb6bbb51ba
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-02-13 15:40:09 +0100

    LinkedList

---
+++ src/LectionLinkedList.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.LinkedList;
+public class LectionLinkedList {
+
+
+
+        public static void main(String[] args) {
+
+            // TODO: create cities ArrayList;
+            List<String> cities = new LinkedList<>();
+
+            cities.add("Paris");
+            cities.add("Florence");
+            cities.add("Venice");
+            cities.add("Versailles");
+            cities.add("London");
+            cities.add("Petra");
+            cities.add("Oslo");
+            cities.add("Rome");
+            cities.add("Madrid");
+            cities.add("Tokyo");
+            cities.add("Prague");
+            cities.add("Dublin");
+            cities.add("Athens");
+            cities.add("Marseille");
+            cities.add("Chicago");
+
+            System.out.println(cities.size());
+            System.out.println(cities.get(5));
+            System.out.println(cities.get(8));
+            cities.set(2, "Naples");
+            cities.remove(5);
+
+
+        }
+
+
+
+
+
+
+}
