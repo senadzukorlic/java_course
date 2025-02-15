42287ba0cca9c8e9e0e8ac1d51e8a14eb3aca5e0
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-02-15 13:56:06 +0100

    My mini projects part 2

--- vezbe/Contact.java
+++ vezbe/Contact.java
@@ -1,23 +1,40 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+
 public class Contact {
     private String name;
     private String phoneNumber;
     private String birthDate;
     private int age;
 
-    public Contact(String name, String phoneNumber, String birthDate, int age){
+    public Contact(String name, String phoneNumber, String birthDate){
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthDate = birthDate;
-        this.age = age;
+        this.age = toAge(birthDate);
     }
 
     public Contact(Contact source){
         this.name = source.name;
         this.phoneNumber = source.phoneNumber;
         this.birthDate = source.birthDate;
-        this.age = source.age;
+    this.age = source.toAge(source.birthDate);
     }
 
+    public int toAge(String BirthDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDate datumRodjenja = LocalDate.parse(BirthDate, formatter);
+        LocalDate danas = LocalDate.now();
+        Period period = Period.between(datumRodjenja, danas);
+        return period.getYears();
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 
     public void printContact (){
         System.out.println("Name: " + this.name + "\nPhone number: " + this.phoneNumber + "\nBirth date: " + this.birthDate + "\nAge: " + this.age);
--- vezbe/MainVezbe.java
+++ vezbe/MainVezbe.java
@@ -1,7 +1,7 @@
 public class MainVezbe {
     public static void main(String[] args){
-        Contact contact1 = new Contact("Alice", "123-456-7890", "1990-01-01", 33);
-        Contact contact2 = new Contact("Bob", "234-567-8901", "1992-02-02", 31);
+        Contact contact1 = new Contact("Alice", "123-456-7890", "1990-01-01");
+        Contact contact2 = new Contact("Bob", "234-567-8901", "1992-02-02");
 
         contact2.printContact();
 
