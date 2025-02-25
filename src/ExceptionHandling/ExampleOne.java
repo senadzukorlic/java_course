88fa6ef50290f7d59f1e50abbabf526f88346a8e
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-02-25 21:03:58 +0100

    Compile-Time Exceptions

--- src/ExceptionHandling/ExampleOne.java
+++ src/ExceptionHandling/ExampleOne.java
@@ -1,7 +1,18 @@
 package ExceptionHandling;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ExampleOne {
     public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("C:\\Users\\PC\\IdeaProjects\\HelloWorld\\src\\ExceptionHandling\\greetings.txt");
+             Scanner scanner = new Scanner(fis);
+             System.out.println(scanner.nextLine());
+             scanner.close();
+        }catch (FileNotFoundException exception){
+            System.out.println(exception.getMessage());
+        }
 
     }
 
--- src/ExceptionHandling/ExampleTwo.java
+++ src/ExceptionHandling/ExampleTwo.java
@@ -1,14 +1,24 @@
 package ExceptionHandling;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ExampleTwo {
     public static void main(String[] args) {
-
+    try {
+    readFile("C:\\Users\\PC\\IdeaProjects\\HelloWorld\\src\\ExceptionHandling\\greetings.txt");
+    } catch (FileNotFoundException exception){
+    System.out.println(exception.getMessage());
+    } finally {
+    System.out.println("This is finally block");
+}
     }
 
-    public static void readFile(String fileName) {
-        // FileInputStream fis = new FileInputStream("greetings.txt");
-        // Scanner scanner = new Scanner(fis);
-        // System.out.println(scanner.nextLine());
-        // scanner.close();
+    public static void readFile(String fileName) throws FileNotFoundException {
+         FileInputStream fis = new FileInputStream(fileName);
+         Scanner scanner = new Scanner(fis);
+         System.out.println(scanner.nextLine());
+         scanner.close();
     }
 }
