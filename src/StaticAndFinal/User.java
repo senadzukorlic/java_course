8ecc94798ac51d3ec5523fe10839fc6623b37ec3
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-03-04 23:34:06 +0100

    static

---
+++ src/StaticAndFinal/User.java
@@ -0,0 +1,35 @@
+package StaticAndFinal;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private static int userCount = 0;
+
+    public User(String firstName, String lastName) {
+        userCount++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public static int getUserCount() {
+        return userCount;
+    }
+
+}
